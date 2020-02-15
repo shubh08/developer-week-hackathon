30998ef4ba98b31fda0cca49c41babea418eb721
Shubham Kumar <shubhamkumar567@gmail.com>
2020-02-15 14:56:55 -0800

    Cleaned starter code

--- Frontend/src/components/UserLoginSignup/LoginDynamic.js
+++
@@ -1,9 +0,0 @@
-document.querySelector('.img__btn').addEventListener('click', function() {
-    document.querySelector('.cont').classList.toggle('s--signup');
-  });
-
-//   window.setTimeout(function() {
-//     $(".alert").fadeTo(500, 0).slideUp(500, function(){
-//         $(this).remove(); 
-//     });
-// }, 2000);
