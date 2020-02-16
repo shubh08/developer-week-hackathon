78348ae5de544e1a2455cf33c1a6e96f80a214db
Nachiket Trivedi <nachiket5197@gmail.com>
2020-02-16 01:47:28 -0800

    displaymap, navbar, baskets

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -1,49 +1,59 @@
-/*!
-
-=========================================================
-* Light Bootstrap Dashboard React - v1.3.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/light-bootstrap-dashboard-react
-* Copyright 2019 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/light-bootstrap-dashboard-react/blob/master/LICENSE.md)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
 import React from "react";
 import ReactDOM from "react-dom";
+import "./index.css";
 
-import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
-
+import * as serviceWorker from "./serviceWorker";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./assets/css/animate.min.css";
-import "./assets/sass/light-bootstrap-dashboard-react.scss?v=1.3.0";
-import "./assets/css/demo.css";
-import "./assets/css/pe-icon-7-stroke.css";
+import "@trendmicro/react-sidenav/dist/react-sidenav.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-import AdminLayout from "layouts/Admin.jsx";
-import CommunityLayout from "layouts/Community";
-import CommunityLogin from "components/Community/CommunityLogin";
-import BusinessLogin from "components/Business/BusinessLogin";
-import BusinessSignup from "components/Business/BusinessSignup";
-import CommunitySignup from "components/Community/CommunitySignup";
+import Dashboard from "./components/Dashboard/Dashboard";
+import Landing from './components/Landing/Landing';
+import Landing1 from './components/Landing/Landing1';
+import Login from "./components/login/Login";
+import Signup from "./components/Signup/Signup";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render(
-  <BrowserRouter>
-    <Switch>
-      <Route path="/admin" render={props => <AdminLayout {...props} />} />
-      {/* <Redirect from="/" to="/admin/dashboard" /> */}
-    <Route path="/community" render={props => <CommunityLayout {...props} />} /> 
-    <Route exact path="/communitylogin" component={CommunityLogin}/>
-    <Route exact path="/communitysignup" component={CommunitySignup}/>
-      <Route exact path="/businesslogin" component={BusinessLogin}/>
-      <Route exact path="/businesssignup" component={BusinessSignup}/>
-    </Switch>
-  </BrowserRouter>,
-  document.getElementById("root")
-);
+/*
+Sarthak Component
+*/
+
+import BusinessHome from './components/BusinessHome/BusinessHome'
+import DisplayMap from './components/DisplayMap/DisplayMap'
+
+class Routes extends React.Component {
+  render() {
+    return (
+      <Router>
+
+        <Route exact path="/" component={Landing} />
+        
+        <Route path="/landing" component={Landing} />
+        <Route path="/landingGooey" component={Landing1} />
+        <Route path="/login" component={Login} />
+        <Route path="/signup" component={Signup} />
+
+        <Route path="/chat" component={() => {
+          if (localStorage.getItem('role') == 'Admin') {
+            window.location.href = "http://localhost:8080/admin?username=" + localStorage.getItem('username') + "&" + "role=" + localStorage.getItem('role') + "";
+            return null;
+          } else {
+            window.location.href = "http://localhost:8080?username=" + localStorage.getItem('username') + "&" + "role=" + localStorage.getItem('role') + "";
+            return null;
+          }
+        }} />
+
+        <Route path = '/businessHome' exact component = {BusinessHome} />
+        <Route path = '/displayMap' exact component = {DisplayMap} />
+
+      </Router>
+
+    );
+  }
+}
+ReactDOM.render(<Routes />, document.getElementById("root"));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
